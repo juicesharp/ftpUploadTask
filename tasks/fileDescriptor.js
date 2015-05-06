3ede9a963f0b0a0c91411f9c1b8e52d06b30300c
Mikael Lindqvist <li.mikael@gmail.com>
2015-05-06 18:49:37 +0200

    checksums file option

--- tasks/fileDescriptor.js
+++ tasks/fileDescriptor.js
@@ -1,5 +1,8 @@
+var sha1=require("sha1");
+var fs=require("fs");
+
 function FileDescriptor(path, dest){
-    var _path, _dest;
+    var _path, _dest, _localSha, _remoteSha;
 
     if(this instanceof FileDescriptor) {
         _path = path;
@@ -15,13 +18,36 @@ function FileDescriptor(path, dest){
     this.getDest = function(){
         return _dest;
     };
+
+    this.getLocalSha = function() {
+        if (_localSha)
+            return _localSha;
+
+        if (fs.statSync(_path).isDirectory())
+            return undefined;
+
+        _localSha=sha1(fs.readFileSync(_path));
+        return _localSha;
+    }
+
+    this.getRemoteSha = function() {
+        return _remoteSha;
+    }
+
+    this.setRemoteSha = function(remoteSha) {
+        _remoteSha=remoteSha;
+    }
 }
 
 FileDescriptor.prototype = {
     constructor : FileDescriptor,
-    getDest : function(){ return this.getDest(); },
-    getPath : function(){ return this.getPath(); },
-    print   : function(){ return this.getPath() + ' --> ' + this.getDest(); }
+    getDest      : function(){ return this.getDest(); },
+    getPath      : function(){ return this.getPath(); },
+    getLocalSha  : function(){ return this.getLocalSha(); },
+    getRemoteSha : function(){ return this.getRemoteSha(); },
+    setRemoteSha : function(sha){ this.setRemoteSha(sha); },
+    shouldSkip   : function(){ return this.getRemoteSha() && (this.getRemoteSha()==this.getLocalSha()); },
+    print        : function(){ return this.getPath() + ' --> ' + this.getDest(); }
 };
 
 module.exports = FileDescriptor;
